9273a344890b8e123a2b9c86af9cefe1625388ed
Antonella Kurday <136640472+antonellakurday@users.noreply.github.com>
2023-10-14 22:17:27 -0300

    Create backend.sql

---
+++ practica backend/backend.sql
@@ -0,0 +1,11 @@
+create table persona (
+dni INT(20) primary key,
+nombre VARCHAR(30) NOT NULL,
+apellido VARCHAR(30) NOT NULL
+);
+
+create table usuario (
+mail varchar (40) primary key,
+nikname varchar(20) not null,
+pasword varchar(20) not null
+);
